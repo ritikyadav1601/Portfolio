97f5fc5e8d14934ace21a6c9da25ef831c9cb196
ritikyadav1601 <124035362+ritikyadav1601@users.noreply.github.com>
2024-06-14 18:48:48 +0530

    Update content_option.js

--- src/content_option.js
+++ src/content_option.js
@@ -1,166 +1,149 @@
-const logotext = "JOHN";
+
+const logotext = "RITIK";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+  title: "Ritik Yadav",
+  description: "I’m Full stack devloper,currently working in Hisar",
 };
 
 const introdata = {
-    title: "I’m John Doe",
-    animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
-    },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+  title: "I’m Ritik Yadav",
+  animated: {
+    first: "I am Full Stack Developer",
+    second: "I code cool websites",
+  },
+  description:
+    "Explore a collection of work where creativity meets precision. Discover how thoughtful design and a client-focused approach result in outstanding achievements.",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
-    title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+  title: "abit about my self",
+  aboutme:
+    "I'm Ritik Yadav, a full stack developer at LCG Hisar. I hold a B.Sc. in Computer Science and have completed the MERN stack course at SSDN Gurugram. With a passion for innovation and a commitment to excellence, I specialize in creating seamless digital solutions. Explore my portfolio to see how I apply my skills in creativity, technical expertise, and a client-centered approach to deliver impactful results.",
 };
-const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
-    },
+const worktimeline = [
+  {
+    jobtitle: "Full Stack Developer",
+    where: "LCG , Hisar",
+    date: "Feb 2024 - Currently",
+  },
+  {
+    jobtitle: "Web Developer Intern",
+    where: "Suvidha Foundation , Mumbai",
+    date: "2023",
+  },
 ];
 
-const skills = [{
-        name: "Python",
-        value: 90,
-    },
-    {
-        name: "Djano",
-        value: 85,
-    },
-    {
-        name: "Javascript",
-        value: 80,
-    },
-    {
-        name: "React",
-        value: 60,
-    },
-    {
-        name: "Jquery",
-        value: 85,
-    },
+const skills = [
+  {
+    name: "HTML",
+    value: 90,
+  },
+  {
+    name: "CSS",
+    value: 85,
+  },
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "React",
+    value: 70,
+  },
+  {
+    name: "Node",
+    value: 65,
+  },
+  {
+    name: "Express",
+    value: 65,
+  },
+  {
+    name: "MongoDB",
+    value: 70,
+  },
 ];
 
-const services = [{
-        title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
+const services = [
+  {
+    title: "Frontend Development",
+    description:
+      "Building user interfaces and experiences using HTML, CSS, JavaScript, and frontend frameworks like React.",
+  },
+  {
+    title: "Backend Development",
+    description:
+      "Writing server-side logic, handling databases, and ensuring seamless integration of frontend components with backend services using technologies like Node.js etc",
+  },
+  {
+    title: "Database Management",
+    description:
+      "Designing and implementing databases, optimizing queries, and ensuring data integrity using NoSQL databases like MongoDB, etc.",
+  },
 ];
 
-const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+const dataportfolio = [
+  {
+    img: "https://i.ibb.co/h8QLQRj/Screenshot-2024-06-14-120132.png", // Ensure this URL is the direct link to the image
+    description:
+      "This crypto react app shows real time data",
+    link: "https://react-crypto-app-puce.vercel.app/",
+  },
+  {
+    img: "https://i.ibb.co/LgfhS5P/Screenshot-2024-06-14-115940.png",
+    description: 
+      "This website show the videos and a option to upload video ",
+    link: "https://react-video-app-five-wine.vercel.app/",
+  },
+  {
+    img: "https://i.ibb.co/z8Hb7Bv/Screenshot-2024-06-14-121957.png",
+    description: 
+      "This is a live website which i develop through a freelencing work",
+    link: "https://hypethegyms.com/",
+  },
+  {
+    img: "https://i.ibb.co/6XbgcJY/Screenshot-2024-06-14-120401.png",
+    description: 
+      "This is a simple website with routing.",
+    link: "https://teamsmart-ten.vercel.app/",
+  },
+  {
+    img: "https://i.ibb.co/7Jj7zsw/Screenshot-2024-06-14-161629.png",
+    description: 
+      "E-Commerce Website with add to cart functionality.",
+    link: "https://e-commerce-lyart-omega-14.vercel.app/",
+  }
+ 
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
-    // creat an emailjs.com account 
-    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
+  YOUR_EMAIL: "rythukran713@gmail.com",
+  YOUR_FONE: "7027977081",
+  description:
+    "Feel free to contact me to discuss potential opportunities, my qualifications, and how I can contribute to your team.",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
+  github: "https://github.com/ritikyadav1601",
+  facebook: "https://facebook.com",
+  linkedin: "https://www.linkedin.com/in/ritik-yadav1601/",
+  twitter: "https://x.com/RitikYa10618907",
 };
 export {
-    meta,
-    dataabout,
-    dataportfolio,
-    worktimeline,
-    skills,
-    services,
-    introdata,
-    contactConfig,
-    socialprofils,
-    logotext,
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
 };
